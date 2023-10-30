c3a298be03f3acd098d7f6bddbf2bdfb11f79b6b
ChoDanbi7 <eksqltpcks77@naver.com>
2023-10-30 14:35:43 +0900

    231030 수정

--- js/common.js
+++ js/common.js
@@ -3,23 +3,23 @@ const getAll = target => document.querySelectorAll(target);
 
 /* topUp btn */
 const topBtnGo = () => {
-    let topBtn = document.getElementById('top_btn');
-    window.addEventListener('scroll', () => {
-      if (document.querySelector('html').scrollTop > 500) {
-        topBtn.style.display = "block";
-      } else {
-        topBtn.style.display = "none";
-      }
+  let topBtn = document.getElementById('top_btn');
+  window.addEventListener('scroll', () => {
+    if (document.querySelector('html').scrollTop > 500) {
+      topBtn.style.display = "block";
+    } else {
+      topBtn.style.display = "none";
+    }
+  });
+  topBtn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
     });
-    topBtn.addEventListener('click', () => {
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-      });
-    })
-  };
-  topBtnGo();
+  })
+};
+topBtnGo();
 
 
 ;( () => {
--- js/sub.js
+++ js/sub.js
@@ -1,13 +1,12 @@
 /* sub1 */
 const sub1Scroll = () => {
     let $subDesc = getAll('.subDesc');
-    let $htmlScroll = get('html');
     window.addEventListener('scroll', () => {
-      if ($htmlScroll.scrollTop > 100) {
-        $subDesc.classList.add('scrollOn');
-      } else {
-        $subDesc.classList.remove('scrollOn');
-      }
+        if (document.querySelector('html').scrollTop > 100) {
+            $subDesc.classList.add('scrollOn');
+        } else {
+            $subDesc.classList.remove('scrollOn');
+        }
     });
-  };
-  sub1Scroll();
+};
+sub1Scroll();
